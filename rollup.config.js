662d06618ba1601bb13be29f13ab8afccead14db
xingkong23333 <1036397001@qq.com>
2023-07-23 12:10:03 +0800

    chore: core-js

--- rollup.config.js
+++ rollup.config.js
@@ -46,7 +46,8 @@ const config = defineConfig([{
         terser(),
         json(),
         eslint({
-            throwOnError: true
+            throwOnError: true,
+            fix: true
         })
     ]
 }, {
